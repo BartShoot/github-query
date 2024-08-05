6a400388e6f32efffe02b196f9217c2fa53fd4b7
Bartosz Dobija <33185107+BartShoot@users.noreply.github.com>
2024-08-06 01:09:11 +0200

    configure object mapper to use SNAKE_CASe

---
+++ src/main/java/com/bartshoot/github_query/config/AppConfiguration.java
@@ -0,0 +1,15 @@
+package com.bartshoot.github_query.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfiguration {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper().setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+    }
+}
