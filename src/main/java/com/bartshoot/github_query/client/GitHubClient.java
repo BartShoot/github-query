f87064d999daabcc65dd27fcf106722b003c0091
Bartosz Dobija <33185107+BartShoot@users.noreply.github.com>
2024-08-06 01:22:10 +0200

    add getRepoBranches to GitHubClient

--- src/main/java/com/bartshoot/github_query/client/GitHubClient.java
+++ src/main/java/com/bartshoot/github_query/client/GitHubClient.java
@@ -1,5 +1,6 @@
 package com.bartshoot.github_query.client;
 
+import com.bartshoot.github_query.models.Branch;
 import com.bartshoot.github_query.models.Repository;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.service.annotation.GetExchange;
@@ -9,4 +10,7 @@ import java.util.List;
 public interface GitHubClient {
     @GetExchange("users/{userName}/repos")
     List<Repository> getUserRepositories(@PathVariable String userName);
+
+    @GetExchange("repos/{userName}/{repoName}/branches")
+    List<Branch> getRepoBranches(@PathVariable String userName, @PathVariable String repoName);
 }
