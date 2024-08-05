a53243344a4021177f6be5a2af9b0efdfc944240
Bartosz Dobija <33185107+BartShoot@users.noreply.github.com>
2024-08-05 19:37:23 +0200

    Initial Spring Boot config

---
+++ src/test/java/com/bartshoot/github_query/GithubQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bartshoot.github_query;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubQueryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
