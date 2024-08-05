2209a55c595b18d40c97ddc2c4e0c25c78f76fa6
Bartosz Dobija <33185107+BartShoot@users.noreply.github.com>
2024-08-06 01:43:26 +0200

    update ClientConfiguration with github api key

--- src/main/java/com/bartshoot/github_query/config/ClientConfiguration.java
+++ src/main/java/com/bartshoot/github_query/config/ClientConfiguration.java
@@ -1,19 +1,28 @@
 package com.bartshoot.github_query.config;
 
 import com.bartshoot.github_query.client.GitHubClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 public class ClientConfiguration {
+
+    @Value("github.api-key")
+    private String gitHubApiKey;
 
     @Bean
     GitHubClient gitHubClient() {
-        RestClient restClient = RestClient.builder().baseUrl("https://api.github.com/").build();
-        RestClientAdapter adapter = RestClientAdapter.create(restClient);
+        RestClient.Builder restClient = RestClient.builder().baseUrl("https://api.github.com/");
+        if (!gitHubApiKey.isBlank()) {
+            restClient.defaultHeader("Authentication", "Bearer " + gitHubApiKey);
+        }
+        RestClientAdapter adapter = RestClientAdapter.create(restClient.build());
         HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(adapter).build();
         return factory.createClient(GitHubClient.class);
     }
