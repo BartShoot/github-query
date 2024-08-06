2f964518c7652d980532f3d456f2a51e8be0291c
Bartosz Dobija <33185107+BartShoot@users.noreply.github.com>
2024-08-06 23:40:24 +0200

    handle pagination

--- src/main/java/com/bartshoot/github_query/clients/GitHubClient.java
+++ src/main/java/com/bartshoot/github_query/clients/GitHubClient.java
@@ -2,15 +2,20 @@ package com.bartshoot.github_query.clients;
 
 import com.bartshoot.github_query.models.Branch;
 import com.bartshoot.github_query.models.Repository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 
 import java.util.List;
 
 public interface GitHubClient {
+
     @GetExchange("users/{userName}/repos")
-    List<Repository> getUserRepositories(@PathVariable String userName);
+    ResponseEntity<List<Repository>> getUserRepositories(@PathVariable String userName,
+                                                         @RequestParam(value = "page") int page);
 
     @GetExchange("repos/{userName}/{repoName}/branches")
-    List<Branch> getRepoBranches(@PathVariable String userName, @PathVariable String repoName);
+    ResponseEntity<List<Branch>> getRepoBranches(@PathVariable String userName, @PathVariable String repoName,
+                                                 @RequestParam(value = "page") int page);
 }
