f02867ff8f6bec71d9037c20d1eb1f4da686ba1e
Bartosz Dobija <33185107+BartShoot@users.noreply.github.com>
2024-09-24 21:24:44 +0200

    allow CORS for development purposes

--- src/main/java/com/bartshoot/github_query/controllers/GithubUserQueryController.java
+++ src/main/java/com/bartshoot/github_query/controllers/GithubUserQueryController.java
@@ -4,6 +4,7 @@ import com.bartshoot.github_query.models.RepositoryFront;
 import com.bartshoot.github_query.validators.ValidGitHubUserName;
 import jakarta.validation.Valid;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 @RequestMapping("/")
+@CrossOrigin(origins = "http://localhost:3000")
 public interface GithubUserQueryController {
 
     @GetMapping("user/{userName}/repos")
