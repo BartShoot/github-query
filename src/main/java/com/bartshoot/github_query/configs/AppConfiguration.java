23f84bf8667dd78c040df9ac75f7855add641ede
Bartosz Dobija <33185107+BartShoot@users.noreply.github.com>
2024-09-24 22:00:22 +0200

    configure cors only local env

--- src/main/java/com/bartshoot/github_query/configs/AppConfiguration.java
+++ src/main/java/com/bartshoot/github_query/configs/AppConfiguration.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class AppConfiguration {
@@ -11,5 +14,16 @@ public class AppConfiguration {
     @Bean
     public ObjectMapper objectMapper() {
         return new ObjectMapper().setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+    }
+
+    @Bean
+    @Profile("local")
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowedHeaders("*");
+            }
+        };
     }
 }
--- src/main/java/com/bartshoot/github_query/controllers/GithubUserQueryController.java
+++ src/main/java/com/bartshoot/github_query/controllers/GithubUserQueryController.java
@@ -4,7 +4,6 @@ import com.bartshoot.github_query.models.RepositoryFront;
 import com.bartshoot.github_query.validators.ValidGitHubUserName;
 import jakarta.validation.Valid;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 @RequestMapping("/")
-@CrossOrigin(origins = "http://localhost:3000")
 public interface GithubUserQueryController {
 
     @GetMapping("user/{userName}/repos")
